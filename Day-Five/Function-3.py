b33987ab81da749055cbfe4d681a0f20a6478ffe
chadX <alamatnicredo@gmail.com>
2026-06-27 22:31:37 +0800

    added files Function-3.py

---
+++ Day-Five/Function-3.py
@@ -0,0 +1,7 @@
+def greetings_function(name , age):
+    
+    print("Hello "+ name + " you are " + str(age) + " years old")
+name= input("Enter your name: ")
+age= input("Enter your age: ")
+
+greetings_function(name, age)
