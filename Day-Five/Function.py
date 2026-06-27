070fc4941fb58976533707c87bc4ec1e043f5db8
chadX <alamatnicredo@gmail.com>
2026-06-27 20:03:36 +0800

    added Function.py

---
+++ Day-Five/Function.py
@@ -0,0 +1,7 @@
+# indentation is important in python
+def greetings_function(name , age):
+    
+    print("Hello "+ name + " you are " + str(age) + " years old")
+
+
+greetings_function("Richard" , 25)
