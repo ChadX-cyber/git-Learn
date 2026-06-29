9e9f66e01b24f61046bec97abee30c34028f1387
chadX <alamatnicredo@gmail.com>
2026-06-29 12:57:31 +0800

    added new folder and  new files

---
+++ Day-Seven/if-5.py
@@ -0,0 +1,9 @@
+boy = True
+short = True
+
+if boy== True and short == True:
+    print("he is a boy or he is short")
+elif boy == False:
+    print("a is false ")
+else:
+    print("a is none of the two")
