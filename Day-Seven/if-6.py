be9bf06f7bf1152b73ea4eb9a3dea82808b99e6a
chadX <alamatnicredo@gmail.com>
2026-06-29 14:40:54 +0800

    added If-6 and If-7.py

---
+++ Day-Seven/if-6.py
@@ -0,0 +1,9 @@
+#  checking the vaule if it is a integer or string
+value=int(input("Input a Value:"))
+
+if type(value) == str:
+    print(value + "is a string")
+
+else:
+    print(value ," is not a string")
+    
---
+++ Day-Seven/if-7.py
@@ -0,0 +1,6 @@
+value = int(input("Input a number:"))
+
+if value%5 == 0:
+    print(value, "is divisible by 5")
+else:
+    print(value, "is not divisible by 5")
