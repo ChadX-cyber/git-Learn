dd434fd5470db4c0164a3d0c4297dc34747775ab
chadX <alamatnicredo@gmail.com>
2026-06-30 16:23:38 +0800

    modified and added new read-write1.py

--- Day-Eight/Read-write.py
+++ Day-Eight/Read-write.py
@@ -3,4 +3,6 @@ file = open("Day-Eight/countries.txt", "r")
 print(file.readline())
 print(file.readline())
 print(file.readlines())
+
+
 file.close()
---
+++ Day-Eight/Read-write1.py
@@ -0,0 +1,3 @@
+file = open("Day-Eight/countries.txt")
+print(file.readlines()[2])
+file.close()
