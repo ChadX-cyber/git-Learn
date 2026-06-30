09ce4f623643c9af9a8ff60a6bd57a0437ee99f7
chadX <alamatnicredo@gmail.com>
2026-06-30 15:11:04 +0800

    added exercises

---
+++ exercise/my Answer Basic-calculator.py
@@ -0,0 +1,46 @@
+
+
+
+num1 = int(input("Enter the first number: " ))
+num2 = int(input("Enter the second number: " ))
+op = input("enter operator + or - or * or /: ")
+
+def add ():
+    if op == "+":
+        print("You choose add and the answer is:", num1+num2)
+    else:
+        print("error")
+
+def subtract():
+    if op == "-":
+        print("You choose subtract and the answer is:", num1-num2)
+
+    else:
+        print("error subtract")
+def multiply():
+    if op == "*":
+        print("You choose Multiply and the answer is: ", num1*num2)
+    else:
+        print("error multiply")
+def division():
+    if op == "/":
+        print("You choose division and the answer is: ", num1/num2)
+    else:
+        print("error division")
+
+def operation():
+    if op =="+":
+        add()
+    elif op == "-":
+        subtract()
+    elif op == "*":
+        multiply()
+    elif op == "/":
+        division()
+    else:
+        print("Error on operator")
+    
+operation()
+
+    
+
