fb38e3205b2defe586d7a60bca6ae0b1917c6ec3
chadX <alamatnicredo@gmail.com>
2026-06-30 13:32:40 +0800

    added new folder Day-eight and dictionary.py

---
+++ Day-Eight/dictionary.py
@@ -0,0 +1,14 @@
+# in dictionary duplicate is not allowed
+dict = {
+    "name": "John",
+    "age": 30,
+    "is_tall": True,
+    "city": "Manila",
+    "nationality": "Filipino",
+    "friends": ["Alice", "Bob", "Charlie"]
+    
+}
+
+x = dict["name"]
+print(dict)
+print(x)
